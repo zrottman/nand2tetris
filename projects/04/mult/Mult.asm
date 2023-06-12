9753d49f134f799822af1c16e01110575e7443ae
zrottman <zrottman@example.com>
2023-06-12 13:47:59 -0700

    Implement and test multiplication program

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,25 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+@R1
+D=M
+@counter
+M=D
+@R2
+M=0
+(LOOP)
+@counter
+D=M
+@END
+D;JEQ
+@R0
+D=M
+@R2
+M=M+D
+@counter
+M=M-1
+@LOOP
+0;JMP
+(END)
+@END
+0;JMP
