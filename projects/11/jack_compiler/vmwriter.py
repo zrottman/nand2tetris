88c9ce3fdd9d2eae7c82db3875cbdafb2bd86f66
zrottman <zrottman@example.com>
2023-09-12 20:09:12 -0700

    Add lookup tables for enums and implement VM writer functions

--- projects/11/jack_compiler/vmwriter.py
+++ projects/11/jack_compiler/vmwriter.py
@@ -9,40 +9,63 @@ class VMWriter:
     input_filename  : str
     output_filename : str = field(init=False)
     write_file      : typing.IO = field(init=False)
+    segment_lookup  : dict[Segment, str] = field(init=False)
+    command_lookup  : dict[Command, str] = field(init=False)
 
     def __post_init__(self):
         self.output_filename = self.create_output_filename(self.input_filename)
         self.writefile = open(self.output_filename, 'w')
+        segment_lookup = {
+                Segment.CONST   : 'constant',
+                Segment.ARG     : 'argument',
+                Segment.LOCAL   : 'local',
+                Segment.STATIC  : 'static',
+                Segment.THIS    : 'this',
+                Segment.THAT    : 'that',
+                Segment.POINTER : 'pointer',
+                Segment.TEMP    : 'temp'
+                }
+        command_lookup = {
+                Command.ADD : 'add',
+                Command.SUB : 'sub',
+                Command.NEG : 'neg',
+                Command.EQ  : 'eq',
+                Command.GT  : 'gt',
+                Command.LT  : 'lt', 
+                Command.AND : 'and',
+                Command.OR  : 'or',
+                Command.NOT : 'not'
+                }
 
     def create_output_filename(self, jack_file):
         return ''.join([os.path.splitext(jack_file)[0], '.z', '.vm'])
 
     def write_push(self, segment: Segment, idx: int):
-        pass
+        self.write_file.write(' '.join(['push', segment_lookup[segment], idx]))
 
     def write_pop(self, segment: Segment, idx: int):
-        pass
+        self.write_file.write(' '.join(['pop', segment_lookup[segment], idx]))
 
     def write_arithmetic(self, command: Command):
-        pass
+        self.write_file.write(command_lookup[command])
 
     def write_label(self, label: str):
-        pass
+        self.write_file.write(' '.join(['label', label]))
 
     def write_goto(self, label: str):
-        pass
+        self.write_file.write(' '.join(['goto', label]))
 
     def write_if(self, label: str):
-        pass
+        self.write_file.write(' '.join(['if', label]))
 
     def write_call(self, name: str, n_args: int):
-        pass
+        self.write_file.write(' '.join(['call', name, str(n_args)]))
 
-    def write_function(self):
-        pass
+    def write_function(self, name: str, n_locals: int):
+        self.write_file.write(' '.join(['function', name, str(n_locals)]))
 
     def write_return(self):
-        pass
+        self.write_file.write('return')
 
     def close(self):
         self.writefile.close()
